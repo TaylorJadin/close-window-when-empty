2ebbce28dff51ff25fd50c7df1a120899ce5d320
Taylor Jadin <jadin.taylor@gmail.com>
2024-06-17 15:35:02 -0500

    fix for leaf types. count all leaves, then count "empty" ones

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Plugin } from 'obsidian';
+import { Plugin, WorkspaceLeaf } from 'obsidian';
 
 export default class CloseWindowWhenEmptyPlugin extends Plugin {
     private initialLoad: boolean = true;
@@ -8,9 +8,26 @@ export default class CloseWindowWhenEmptyPlugin extends Plugin {
     }
 
     handleLayoutChange = () => {
-        const markdownLeaves = this.app.workspace.getLeavesOfType('markdown');
-        const graphLeaves = this.app.workspace.getLeavesOfType('graph');
-        const leaves = [...markdownLeaves, ...graphLeaves];
+        let numOpenTabs = 0;
+    
+        // Count the number of open tabs
+        this.app.workspace.iterateAllLeaves((leaf: WorkspaceLeaf) => {
+            if (leaf.getRoot() === this.app.workspace.rootSplit) {
+                numOpenTabs++;
+            }
+        });
+
+        // Count the number of empty tabs
+        const emptyLeaves = this.app.workspace.getLeavesOfType('empty');
+
+        // Debug stuff
+        // const activeLeaf = this.app.workspace.activeLeaf;
+        // console.log('Number of open tabs in main view:', numOpenTabs);
+        // if (activeLeaf) {
+        //     console.log('Active leaf type:', activeLeaf.view.getViewType());
+        // } else {
+        //     console.log('No active leaf');
+        // }
 
         // Avoid closing the window on the initial load
         if (this.initialLoad) {
@@ -18,7 +35,8 @@ export default class CloseWindowWhenEmptyPlugin extends Plugin {
             return;
         }
 
-        if (leaves.length === 0) {
+        // Close the window if there is only one tab and the active leaf type is "empty"
+        if (numOpenTabs === 1 && (emptyLeaves.length === 1)) {
             // Close the Obsidian window
             window.close();
         }
